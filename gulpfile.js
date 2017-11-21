9b5bb9d6e03ba83271fe7b4115635a2a15ba35f9
matan <p7586381@pituah.iaf>
2017-11-21 12:16:02 +0200

    dev/gulp: compiling sub directories too

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,18 @@ var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 
 var paths = {
-    scripts: [ "src/*.ts" ],
-    dist: ["dist"]
+    scripts: [ "src/*.ts", "src/**/*.ts" ],
+    dist: "dist"
 }
 
-gulp.task("default", ["scripts", "watch"], function(){
+gulp.task("default", ["scripts", "watch"], () => {
     console.log('gulp out');
 });
 
 gulp.task('scripts', function(){    
     return tsProject.src()
-    .pipe(tsProject())
-    .js.pipe(gulp.dest("dist"));
+    .pipe(tsProject())    
+    .js.pipe(gulp.dest(paths.dist));
 })
 
 gulp.task('watch', function(){
