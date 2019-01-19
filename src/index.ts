c0fab06165b499d6dbbe3de75a4bba51f4e6d84d
givo <matangivoni@gmail.com>
2019-01-20 00:46:36 +0200

    [FIX] fix index imports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export { EndpointWrapper } from './abstract/endpoint-wrapper';
-export { SdpEndpointWrapper } from './abstract/sdp-endpoint-wrapper';
-export { PlayerEndpointWrapper } from './endpoints/player-endpoint-wrapper';
-export { RecorderEndpointWrapper } from './endpoints/recorder-endpoint-wrapper';
-export { RtpEndpointWrapper } from './endpoints/rtp-endpoint-wrapper';
-export { WebRtcEndpointWrapper } from './endpoints/webrtc-endpoint-wrapper';
+export { EndpointWrapper } from './abstract/endpoint/endpoint-wrapper';
+export { SdpEndpointWrapper } from './abstract/sdp/sdp-endpoint-wrapper';
+export { PlayerEndpointWrapper } from './endpoints/player/player-endpoint-wrapper';
+export { RecorderEndpointWrapper } from './endpoints/recorder/recorder-endpoint-wrapper';
+export { RtpEndpointWrapper } from './endpoints/rtp/rtp-endpoint-wrapper';
+export { WebRtcEndpointWrapper } from './endpoints/webrtc/webrtc-endpoint-wrapper';
 export { connectToKurentoServer, createPipeline } from './utils/kurento-utils';
