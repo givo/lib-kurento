c49290e80890ad3e714496ebaeb1e6130aa24f44
givo <matangivoni@gmail.com>
2019-01-19 18:13:33 +0200

    [FEATURE] create index file

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { EndpointWrapper } from './abstract/EndpointWrapper';
+export { SdpEndpointWrapper } from './abstract/SdpEndpointWrapper';
+export { PlayerEndpointWrapper } from './endpoints/PlayerEndpointWrapper';
+export { RecorderEndpointWrapper } from './endpoints/RecorderEndpointWrapper';
+export { RtpEndpointWrapper } from './endpoints/RtpEndpointWrapper';
+export { WebRtcEndpointWrapper } from './endpoints/WebRtcEndpointWrapper';
+export { connectToKurentoServer, createPipeline } from './utils/kurento-utils';
