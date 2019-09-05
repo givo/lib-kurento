dc895c048d7274b05f6df6c87ee4d718ef941421
Jonas <jonas007@vorsicht-bissig.de>
2019-09-05 14:07:20 +0200

    Copy the interface definition in to the extended interface
    This is at the moment the only working solution to allow function overload in an inherited interface
    
    https://github.com/microsoft/TypeScript/issues/10229#issuecomment-238610372

--- src/endpoints/webrtc/webrtc-endpoint.interface.ts
+++ src/endpoints/webrtc/webrtc-endpoint.interface.ts
@@ -1,5 +1,12 @@
 import { IEndpointWrapper } from "../../abstract/endpoint/endpoint-wrapper.interface";
 
 export interface IWebRTCEndpointWrapper extends IEndpointWrapper{
+    on(event: "MediaFlowingIn", listener: (event: any) => void): this;
+
+    on(event: "MediaStoppedFlowingIn", listener: (event: any) => void): this;
+
+    on(event: "MediaFlowingOut", listener: (event: any) => void): this;
+
+    on(event: "MediaStoppedFlowingOut", listener: (event: any) => void): this;
     on(event: "ServerIceCandidate", listener: (candidate: any) => void): this;
 }
