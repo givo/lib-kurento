01a8e95845a6c48607cd87c431dd58768a705551
Matan Givoni <matangivoni@gmail.com>
2019-09-13 20:49:47 +0300

    [FIX] function overloading error

--- src/endpoints/recorder/recorder-endpoint.interface.ts
+++ src/endpoints/recorder/recorder-endpoint.interface.ts
@@ -1,6 +1,14 @@
 import { IEndpointWrapper } from "../../abstract/endpoint/endpoint-wrapper.interface";
 
 export interface IRecorderEndpointWrapper extends IEndpointWrapper {
+    on(event: "MediaFlowingIn", listener: (event: any) => void): this;
+
+    on(event: "MediaStoppedFlowingIn", listener: (event: any) => void): this;
+
+    on(event: "MediaFlowingOut", listener: (event: any) => void): this;
+
+    on(event: "MediaStoppedFlowingOut", listener: (event: any) => void): this;
+    
     on(event: "RecordingStarted", listener: (event: any) => void): this;
 
     on(event: "RecordingStopped", listener: (event: any) => void): this;
